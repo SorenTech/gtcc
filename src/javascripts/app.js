e017c94f69b988ee49bbcb97dcda71395696f980
apmarshall <apmarshall@soren.tech>
2018-08-10 10:00:20 -0400

    Feat: Adding long versions of bio. Failed at first attempt to do this via modal, opting for details panel for now

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,3 +1,22 @@
 import './modules'
 
 console.log(`app.js has loaded!`)
+
+/*
+import swal from 'sweetalert';
+
+var bethBioLink = document.getElementById("beth-bio-long");
+var melissaBioLink = document.getElementById("melissa-bio-long");
+
+bethBioLink.onclick = swal({
+    title: "Beth Armstrong, Director",
+    text: "Beth Armstrong has taught at Roger Williams University, chaired the choral department at the Music School of the Rhode Island Philharmonic, and coordinated both the Junior Providence Singers and the JPS Treble Chorus. She is a graduate of Tufts University and pursued a master’s degree in Music Education from the University of New Hampshire. Prior to living in Rhode Island, Beth directed the Suncook Valley Chorale in Concord, New Hampshire and taught public school music for twelve years. In addition to directing our chorus, she also directs the Chorus of East Providence.",
+    button: "X"
+  });
+  
+  melissaBioLink.onclick = swal({
+    title: "Melissa Woolverton, Accompanist",
+    text: "Melissa Woolverton was born and raised in Manitoba, Canada and at the age of three began studying classical piano at the Royal Conservatory of Music, Canada. She graduated summa cum laude from Northern State University in Aberdeen, South Dakota with a piano, voice, and theater emphasis. Melissa was brought to Rhode Island by the Beechwood Theatre Company in Newport in 2004 where she both acted and served as Music Director. Melissa has been the music director for Newport Children’s Theatre, an accompanist and Music Director for Salve Regina University’s Opera Workshop, the University of Rhode Island, Opera Providence, Newport Children’s Theatre, Suzanne Vega, Dunkan Sheik, and Gerry Leonard, and for Broadway auditions. In addition to serving as our accompanist, she is currently the accompanist for the Chorus of Westerly, the Newport County Youth Chorus, the United Congregational Church in Middletown, RI, the High School Choruses of Middletown, RI, and Mount Hope in Bristol, RI, and for Salve Regina University.",
+    button: "X"
+  });
+*/
